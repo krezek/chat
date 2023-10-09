a0e106f0664926d5f56f8e716371bf39c506c1f1
Kinaz Rezek <kinaz_rezek@hotmail.com>
2023-10-09 09:43:19 +0200

    remove sqlite

--- Server/MIDL/create.cmd
+++ Server/MIDL/create.cmd
@@ -2,10 +2,10 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\V
 
 midl /acf game.acf /env amd64 game.idl
 
-copy game.h ..\include\game.h
-copy game_s.c ..\src\game_s.c
+xcopy /Y game.h ..\include\game.h
+xcopy /Y game_s.c ..\src\game_s.c
 
-copy game.h ..\..\Client\include\game.h
-copy game_c.c ..\..\Client\src\game_c.c
+xcopy /Y game.h ..\..\Client\include\game.h
+xcopy /Y game_c.c ..\..\Client\src\game_c.c
 
 pause
