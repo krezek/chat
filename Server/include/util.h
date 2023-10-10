c0d07fc0ee28003d44c20fe001b8ade1739c92a9
Kinaz Rezek <kinaz_rezek@hotmail.com>
2023-10-10 09:50:00 +0200

    adding Node structure

--- Server/include/util.h
+++ Server/include/util.h
@@ -1,6 +1,19 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include <wchar.h>
 
+typedef struct _Node
+{
+	wchar_t* _key;
+	void* _value;
+
+	struct _Node* _left;
+	struct _Node* _right;
+
+} Node;
+
+Node* Node_init(const wchar_t* k, void* v, Node* l, Node* r);
+void Node_free(Node* n);
 
 #endif /* _UTIL_H_ */
--- Server/src/util.c
+++ Server/src/util.c
@@ -1,2 +1,29 @@
-#include <util.h>
+#include <stdlib.h>
+#include <assert.h>
 
+#include "util.h"
+
+
+Node* Node_init(const wchar_t* k, void* v, Node* l, Node* r)
+{
+	Node* n = (Node*)malloc(sizeof(Node));
+	assert(n != NULL);
+
+	n->_key = (wchar_t*)malloc((wcslen(k) + 1) * sizeof(wchar_t));
+	assert(n->_key != NULL);
+	wcscpy_s(n->_key, wcslen(k) + 1, k);
+
+	n->_value = v;
+	n->_left = l;
+	n->_right = r;
+
+	return n;
+}
+
+void Node_free(Node* n)
+{
+	if (n->_key)
+		free(n->_key);
+
+	free(n);
+}
